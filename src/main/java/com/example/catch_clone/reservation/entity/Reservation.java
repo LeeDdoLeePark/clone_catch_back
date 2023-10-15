7365b1b4ff585064e96e3e41b858a76ff961c917
PriceHoon <saog3151@gmail.com>
2023-10-15 22:12:18 +0900

    #3-1 Reservation.java Add

--- src/main/java/com/example/catch_clone/reservation/entity/Reservation.java
+++ src/main/java/com/example/catch_clone/reservation/entity/Reservation.java
@@ -1,8 +1,10 @@
 package com.example.catch_clone.reservation.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,5 +15,17 @@ public class Reservation {
   @Id
   @GeneratedValue
   private Long id;
+
+  @Column
+  private Long userId;  //회원ID
+
+  @Column
+  private Long storeId; //가맹점ID
+
+  @Column
+  private String reservationStatus;  //예약상태
+
+  @Column
+  private LocalDateTime createdAt;  //생성일자
 
 }
