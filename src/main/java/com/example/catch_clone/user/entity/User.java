58ac538b15c7858aee293a663e0d71d7346ac74c
0empty <2zeroempty@gmail.com>
2023-10-23 19:43:13 +0900

    #10 <fix> 컴파일시 필요한 메서드, 필드 추가 및 수정(임시)

--- src/main/java/com/example/catch_clone/user/entity/User.java
+++ src/main/java/com/example/catch_clone/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.example.catch_clone.user.entity;
 
+import com.example.catch_clone.util.enums.UserRoleEnum;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,5 +17,15 @@ public class User {
   @Id
   @GeneratedValue
   private Long id;
+
+  @Column(length = 25, nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column
+  @Enumerated(EnumType.STRING)
+  private UserRoleEnum role;
 
 }
