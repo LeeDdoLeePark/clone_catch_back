8897d161770cd7644362bec81acf358db7be10d4
0empty <2zeroempty@gmail.com>
2023-10-28 17:30:20 +0900

    #8 <feat> 생성일자, 수정일자 자동 생성을 위한 TimeStamped추가와 설정

--- src/main/java/com/example/catch_clone/CatchCloneApplication.java
+++ src/main/java/com/example/catch_clone/CatchCloneApplication.java
@@ -2,8 +2,11 @@ package com.example.catch_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CatchCloneApplication {
 
 	public static void main(String[] args) {
