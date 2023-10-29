d40b014a01f6afbc5ed796a82e00828024ca3f2b
0empty <2zeroempty@gmail.com>
2023-10-29 20:34:22 +0900

    #8 - 2 <fix> JpaRepository -> Repository로 변경

--- src/main/java/com/example/catch_clone/review/dao/ReviewRepository.java
+++ src/main/java/com/example/catch_clone/review/dao/ReviewRepository.java
@@ -2,9 +2,13 @@ package com.example.catch_clone.review.dao;
 
 import com.example.catch_clone.review.entity.Review;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
 
-public interface ReviewRepository extends JpaRepository<Review,Long> {
+public interface ReviewRepository extends Repository<Review,Long> {
+  void save(Review review);
+  Optional<Review> findById(Long reviewId);
   List<Review> findAllByStoreId(Long storeId);
   List<Review> findAllByUserId(Long userId);
+  void deleteById(Long reviewId);
 }
